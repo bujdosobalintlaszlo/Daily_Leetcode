19325744f07efc399b591cd9e4bde4f1c634f695
Balint <118969351+bujdosobalintlaszlo@users.noreply.github.com>
2025-11-01 12:48:31 +0100

    Create delete-nodes-from-linked-list-present-in-array.cpp

---
+++ delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> mpp(nums.begin(), nums.end());
+
+        //going till we find a head element which isnt in nums
+        while (head && mpp.count(head->val))
+            head = head->next;
+
+
+        ListNode* curr = head;
+        //not the current but the next element(s) in the nums, so we go till we find one which is not or we reach the end
+        while (curr && curr->next) {
+            while (curr->next && mpp.count(curr->next->val)) {
+                curr->next = curr->next->next;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
